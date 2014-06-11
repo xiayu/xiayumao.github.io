aa3e35260b594724bbced18935408b0f6f8fc925
xiayumao <xiayu977848513@gmail.com>
2014-06-11 11:45:42 +0800

    v1

---
+++ story.cpp
@@ -0,0 +1,29 @@
+/**
+* We are both Fudan SSers and programmers,
+* so I write some code to celebrate our 1st anniversary.
+*/
+Boy i = new Boy("liuyu");
+Girl u = new Girl("suxiaying");
+//Dec 18, 2008, I told you I love you. 
+i.love(u);
+// Luckily, you accepted and became my girlfriend eversince.
+u.accepted();
+// Since then, I miss u every day.
+for(int i=0;i<forever;i++){
+	i.miss(u);
+}
+//Since then,I love u every day.
+for(int i=0;i<forever;i++){
+    i.love(u);
+}
+// And take care of u and our love.
+i.takeCareOf(u);
+// But sometimes something goes wrong
+while(bug=(i&u).hasbug()){
+	//i fix it
+	i.fix(bug);
+}
+//Happiness continue
+i.loveHappilyWith(u);
+//love story To be continued
+lovestory.toBeContinued();
